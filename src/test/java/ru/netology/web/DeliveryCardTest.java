7154603f6bf1dcc4f50b6a99e59e6b8fde5f2c4c
Сергей Ивеснкий <ivenskijsv@gmail.com>
2022-08-28 13:39:30 +0300

    Fixed test

--- src/test/java/ru/netology/web/DeliveryCardTest.java
+++ src/test/java/ru/netology/web/DeliveryCardTest.java
@@ -44,7 +44,6 @@ public class DeliveryCardTest {
         $("span[class='checkbox__box']").click();
         $(".button_view_extra.button_size_m").click();
         $("div[data-test-id=success-notification] div[class='notification__content']").shouldHave(Condition.text("Встреча успешно запланирована на " + planningDate1)).shouldBe(Condition.visible);
-        open("http://localhost:9999/");
         $("span[data-test-id='city'] input").setValue("Севастополь");
         $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
         $("span[data-test-id='date'] input[placeholder]").setValue(planningDate2);
@@ -56,5 +55,4 @@ public class DeliveryCardTest {
         $("div[data-test-id=replan-notification] button").click();
         $("div[data-test-id=success-notification] div[class='notification__content']").shouldHave(Condition.text("Встреча успешно запланирована на " + planningDate2)).shouldBe(Condition.visible);
     }
-
 }
