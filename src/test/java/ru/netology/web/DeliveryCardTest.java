e5ba7193c2af255dae5fe4fb9fa985e83d89c2da
Сергей Ивеснкий <ivenskijsv@gmail.com>
2022-08-28 23:55:38 +0300

    Added to GitHab app-card-delivery.jar

--- src/test/java/ru/netology/web/DeliveryCardTest.java
+++ src/test/java/ru/netology/web/DeliveryCardTest.java
@@ -44,6 +44,7 @@ public class DeliveryCardTest {
         $("span[class='checkbox__box']").click();
         $(".button_view_extra.button_size_m").click();
         $("div[data-test-id=success-notification] div[class='notification__content']").shouldHave(Condition.text("Встреча успешно запланирована на " + planningDate1)).shouldBe(Condition.visible);
+        open("http://localhost:9999/");
         $("span[data-test-id='city'] input").setValue("Севастополь");
         $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
         $("span[data-test-id='date'] input[placeholder]").setValue(planningDate2);
